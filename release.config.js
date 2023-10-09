05d8bf6b287056263400d5d7cd9ae0267a69cce5
kaylascho <orimoloyekayode@yahoo.com>
2023-10-08 21:48:02 -0400

    fix: Semantic release

--- release.config.js
+++ release.config.js
@@ -1,15 +1,10 @@
-const config = {
-  branches: ['main'],
+module.exports = {
+  branches: ["main"],
+  repositoryUrl: "https://github.com/kayla1000/s3-module-ticket.git",
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ["@semantic-release/git", {
-      "assets": ["dist/*.js"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],
-    '@semantic-release/github',
-    'semantic-release-github-actions-tags'
+    '@semantic-release/git',
+    '@semantic-release/github'
   ]
 };
-
-module.exports = config;
